9b527f373d5433816e058b03d9e5c5c5109e5594
salemhabte <130196453+salemhabte@users.noreply.github.com>
2026-07-15 18:12:05 +0300

    Fix typo in divide test assertion

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,6 +14,6 @@ def test_multiply():
     assert multiply(-2, 3) == -6
 
 def test_divide():
-    assert divide(6, 2) == 3.o
+    assert divide(6, 2) == 3.0
     with pytest.raises(ValueError):
         divide(5, 0)
